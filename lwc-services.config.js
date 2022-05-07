14c498f5393d8a01571ceb58122d860fc74a5110
Natalia <59320029+missNobody17@users.noreply.github.com>
2022-05-07 17:30:58 +0100

    message

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -2,12 +2,12 @@
 // https://github.com/muenzpraeger/create-lwc-app/blob/main/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
     resources: [
-        { from: 'src/client/resources/', to: 'dist/resources/'},
-        { from: 'src/calculateCorr.js', to: 'dist/calculateCorr.js' },
-        { from: 'src/ftpAccess.js', to: 'dist/ftpAccess.js' },
-        { from: 'src/points.js', to: 'dist/points.js' },
-        { from: 'src/vlfdata.js', to: 'dist/vlfdata.js' },
-        { from: 'src/VLF-data', to: 'dist/VLF-data' }
+        { from: 'src/server/client/resources/', to: 'dist/resources/'},
+        { from: 'src/server/calculateCorr.js', to: 'dist/calculateCorr.js' },
+        { from: 'src/server/ftpAccess.js', to: 'dist/ftpAccess.js' },
+        { from: 'src/server/points.js', to: 'dist/points.js' },
+        { from: 'src/server/vlfdata.js', to: 'dist/vlfdata.js' },
+        { from: 'src/server/VLF-data', to: 'dist/VLF-data' }
       ],
 
     sourceDir: './src/client',
